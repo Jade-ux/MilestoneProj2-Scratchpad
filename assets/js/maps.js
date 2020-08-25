16d0672e004be35690da8ae186f5d203f32ef951
Jade-ux <jadeemailsandbox@gmail.com>
2020-08-25 06:53:32 +0000

    Trying the xhr request

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -1,10 +1,110 @@
 "use strict";
 
-var map;
+/*var map;
 var center;
-var location;
+var location;*/
 
-//The following is from Google API Docs
+//----------------------------------------------------------------------Test 1
+//Taken from: https://chromatichq.com/blog/implementing-google-places-autocomplete-es6-part-one It does not seem to work
+/*
+
+document.addEventListener('DOMContentLoaded', function() {
+
+const autocompleteFormField = document.getElementById(`start-here`);
+
+const autocomplete = new google.maps.places.Autocomplete((autocompleteFormField), {
+  types: [`address`],
+  componentRestrictions: [`GB`],
+
+});
+
+
+google.maps.event.clearInstanceListeners(autocompleteFormField);
+google.maps.event.addListener(autocomplete, ‘place_changed’, function() => {
+  const place = autocomplete.getPlace();
+}
+
+});
+*/
+
+//------------------------------------------------Taken from Resume Project - attempt to get teh value from the starting point input box
+/*
+function fetchPlaceInformation(event){
+   $("#starting-point").html("");
+   var startingPoint = $("#starting-point").val();
+}
+*/
+
+//--------------------------------------------Map with autocomplete box at the top from https://developers.google.com/maps/documentation/javascript/examples/places-searchbox
+
+function initAutocomplete() {
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: {
+      lat: -33.8688,
+      lng: 151.2195
+    },
+    zoom: 13,
+    mapTypeId: "roadmap"
+  }); // Create the search box and link it to the UI element.
+
+  const input = document.getElementById("starting-point");
+  const searchBox = new google.maps.places.SearchBox(input);
+  map.controls[google.maps.ControlPosition.TOP_LEFT].push(input); // Bias the SearchBox results towards current map's viewport.
+
+  map.addListener("bounds_changed", () => {
+    searchBox.setBounds(map.getBounds());
+  });
+  let markers = []; // Listen for the event fired when the user selects a prediction and retrieve
+  // more details for that place.
+
+  searchBox.addListener("places_changed", () => {
+    const places = searchBox.getPlaces();
+
+    if (places.length == 0) {
+      return;
+    } // Clear out the old markers.
+
+    markers.forEach(marker => {
+      marker.setMap(null);
+    });
+    markers = []; // For each place, get the icon, name and location.
+
+    const bounds = new google.maps.LatLngBounds();
+    places.forEach(place => {
+      if (!place.geometry) {
+        console.log("Returned place contains no geometry");
+        return;
+      }
+
+      const icon = {
+        url: place.icon,
+        size: new google.maps.Size(71, 71),
+        origin: new google.maps.Point(0, 0),
+        anchor: new google.maps.Point(17, 34),
+        scaledSize: new google.maps.Size(25, 25)
+      }; // Create a marker for each place.
+
+      markers.push(
+        new google.maps.Marker({
+          map,
+          icon,
+          title: place.name,
+          position: place.geometry.location
+        })
+      );
+
+      if (place.geometry.viewport) {
+        // Only geocodes have viewport.
+        bounds.union(place.geometry.viewport);
+      } else {
+        bounds.extend(place.geometry.location);
+      }
+    });
+    map.fitBounds(bounds);
+  });
+}
+//--------------------------------------------The following is from Google API Docs
+/*
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: {
@@ -14,27 +114,9 @@ function initMap() {
     zoom: 13
     
   }); 
-}
-
-
-//----------------------------------------------------------------------Test 1
-//Taken from: https://chromatichq.com/blog/implementing-google-places-autocomplete-es6-part-one
-
-document.addEventListener('DOMContentLoaded', function() {
-
-const autocompleteFormField = document.getElementById(`street-
-address-field`);
-
-const autocomplete = new google.maps.places.Autocomplete((autocompleteFormField), {
-  types: [`address`],
-  /*componentRestrictions: [`GB`],*/
-
-});
-/*
-google.maps.event.clearInstanceListeners(autocompleteFormField);
-google.maps.event.addListener(autocomplete, ‘place_changed’, function() => {
-  const place = autocomplete.getPlace();
 }*/
+
+
 
 //----------------------------------------------------------------------Test 2
 //The following is from https://gist.github.com/code-nation/eaf1fc1687949727f5f5
@@ -54,16 +136,16 @@ google.maps.event.addListener(autocomplete, ‘place_changed’, function() => {
     // Uncomment the line below if you wish to call the fillInAddress example function on user selection
     autocomplete.addListener('place_changed', fillInAddress);
   }
-//1. End of test */
+//2. End of test */
 
 
 
-  google.maps.event.addDomListener(window, 'load', initAutocomplete);
-
-});
+  //google.maps.event.addDomListener(window, 'load', initAutocomplete);
 
 
 
+
+//-----------------------------------------------------------------------Test 3
 //The following code is taken from: https://www.youtube.com/watch?v=zVU_MQyKFGg
  /*function initialize() {
      var center = new google.maps.LatLng(51.8994, -2.0783);
@@ -101,6 +183,20 @@ function createMarker(place) {
 
 google.maps.event.addDomListener(window, 'load', initialize);*/
 
+
+//XHR taken from Code Institute video: https://www.youtube.com/watch?v=mq9buYAnJ74&feature=youtu.be error - request denied
+
+var xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("data").innerHTML = this.responseText; 
+        }
+};
+
+xhr.open("GET", "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&key=AIzaSyAbcTamNPI1Pe4MAqwMqq6JYO3z_zwwwuY");
+
+xhr.send();
 
 //write function that pulls the input from the input box and sets it as the variable startingPoint, replacing the value for location in the function above with the starting point
 //Also right a function that pulls the 'distance from' and sets it as the radius in the above.
